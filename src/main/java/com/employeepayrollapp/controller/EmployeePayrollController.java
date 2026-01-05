90cc3cfb3e36c6c5fe6a2c11217468cd06f35363
onizukaTP <tharunprabu2418@gmail.com>
2026-01-05 15:25:05 +0530

    [tp][day4] UC2: created a rest controller to demonstrate various http methods

--- src/main/java/com/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/employeepayrollapp/controller/EmployeePayrollController.java
@@ -1,4 +1,37 @@
 package com.employeepayrollapp.controller;
 
+import com.employeepayrollapp.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/emp")
 public class EmployeePayrollController {
+    private static final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/get")
+    public String testGet() {
+        return "Hello world";
+    }
+
+    @PostMapping("/post")
+    public Employee testPost(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Employee(counter.incrementAndGet(), name);
+    }
+
+    @PutMapping("/put/{id}")
+    public String testPut(
+            @PathVariable Long id,
+            @RequestBody Employee employee
+    ) {
+        return "Employee " + id + " updated with name: " + employee.getName();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String testDelete(@PathVariable Long id) {
+        return "Employee " + id + " got yeeted!";
+    }
+
 }
