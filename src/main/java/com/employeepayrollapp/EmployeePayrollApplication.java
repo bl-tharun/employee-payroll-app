d21c45b93189cde25304a72567b525437b47791d
onizukaTP <tharunprabu2418@gmail.com>
2026-01-06 17:00:55 +0530

    [tp][day6] UC2: used lombok library for logging

---
+++ src/main/java/com/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,18 @@
+package com.employeepayrollapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class);
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+    }
+
+}
