581c1af7fe3c7f753215dfc2a195ce3d50b1eda7
onizukaTP <tharunprabu2418@gmail.com>
2026-01-07 16:57:49 +0530

    [tp][day7] UC2: added validation for added attributes

--- src/main/java/com/employeepayrollapp/dto/EmployeePayrollDto.java
+++ src/main/java/com/employeepayrollapp/dto/EmployeePayrollDto.java
@@ -1,25 +1,28 @@
 package com.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Data;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public @Data class EmployeePayrollDto {
-
     public int empId;
-
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
-
     @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
-
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "StartDate should not be empty")
+    @PastOrPresent(message = "StartDate should be past or today's date")
     public LocalDate startDate;
+    @NotBlank(message = "Note cannot be empty")
     public String note;
+    @NotBlank(message = "ProfilePic cannot be empty")
     public String profilePic;
+    @NotNull(message = "Departments cannot be empty")
     public List<String> departments;
 }
--- src/main/java/com/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
@@ -4,6 +4,7 @@ import com.employeepayrollapp.dto.ResponseDto;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class EmployeePayrollExceptionHandler {
+    private static final String message = "Exception while processing REST request";
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException exception
@@ -22,7 +24,7 @@ public class EmployeePayrollExceptionHandler {
         List<String> errMsg = errorList.stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
-        ResponseDto responseDto = new ResponseDto("Exception while processing REST request", errMsg);
+        ResponseDto responseDto = new ResponseDto(message, errMsg);
         return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 
@@ -30,8 +32,15 @@ public class EmployeePayrollExceptionHandler {
     public ResponseEntity<ResponseDto> handleEmployeePayrollException(
             EmployeePayrollException exception
     ) {
-        ResponseDto responseDto = new ResponseDto("Exception while processing REST request",
-                exception.getMessage());
+        ResponseDto responseDto = new ResponseDto(message, exception.getMessage());
+        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDto> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException exception
+    ) {
+        ResponseDto responseDto = new ResponseDto(message, "Should have correct date formate dd MM yyyy");
         return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 }
